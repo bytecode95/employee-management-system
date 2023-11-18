1e48bab8f85e14f22e62a738f94cf9ee2e574ab2
chethanavirajini <cheth.virajini@gmail.com>
2023-11-18 14:12:27 +0530

    updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.bytetalking.employe_management_systme {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.bytetalking.employe_management_systme to javafx.fxml;
